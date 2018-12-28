56b46cbde31083c672eb705b555b1a5e9d29df3a
Austin <austinninja362@gmail.com>
2018-12-27 22:49:52 -0500

    Changed photo & added pdf

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,11 +9,17 @@ class HomePage extends React.Component{
 	return(
 	    <div>
 	    	<section id="cardHome">
-	    		<img id="inline" height="500px" width="500px" src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/29196576_231189960786850_2050064795070801629_n.jpg?_nc_cat=100&_nc_ht=scontent-lga3-1.xx&oh=dfc690986b55d63480ec719f6e524fc6&oe=5C9F422B" alt="Portrait"/>
-	    		<div id="vertical">
+	    		<center>
+	    			<img id="inline" src="https://github.com/ageor0114/personal-portfolio/blob/master/src/pages/austin.JPG?raw=true"  alt="Portrait"/>
+	    			<br/>
+	    			<br/>
+	    			<h1 id="title">Austin George</h1>
+	    			<Button size="large" variant="outlined" target="_blank" href="https://drive.google.com/open?id=13rWCcGhvS5dPUpJBb3hGSH_SZG5ykmHJdMmNW2BqXbw">See Resume</Button>
+	    		</center>
+	    		{/*<div id="vertical">
 	    			<center><h1 id="title">Austin George</h1></center>
 	    			<center><Button variant="outlined" target="_blank" href="https://drive.google.com/open?id=13rWCcGhvS5dPUpJBb3hGSH_SZG5ykmHJdMmNW2BqXbw">See Resume</Button></center>
-	    		</div>
+	    		</div>*/}
 	    	</section>
 	    	<section id="cardAbout">
 	    		<h1 id="title">About Section</h1>
